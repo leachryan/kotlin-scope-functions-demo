71d35276d7dcec44619362e089b3809188039a87
Ryan Leach <rleach@bushelpowered.com>
2021-05-05 12:18:50 -0500

    Adds tests showcasing the with scope function

--- src/test/kotlin/WithScopeFunctionTest.kt
+++ src/test/kotlin/WithScopeFunctionTest.kt
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class WithScopeFunctionTest {
+
+    @Test
+    fun `test with scope function by calling other functions on object context`() {
+        val joe = Person("Joe Rogan", 53, "Podcast Host", "California")
+        var jreHostName = "Jamie"
+        with(joe) {
+            jreHostName = name
+            relocate("Austin")
+        }
+
+        assertEquals("Austin", joe.location)
+        assertEquals("Joe Rogan", jreHostName)
+    }
+
+    @Test
+    fun `test with scope function by returning value from lambda`() {
+        val elon = Person("Elon Musk", 49, "CEO", "California")
+        val expected = "Name: Elon Musk, Age: 49, Occupation: CEO, Location: California"
+        val summary = with(elon) {
+            "Name: $name, Age: $age, Occupation: $occupation, Location: $location"
+        }
+
+        assertEquals(expected, summary)
+    }
 }
