6f5bbbb9e195a184a1ee570b168ebd939603c095
Ryan Leach <rleach@bushelpowered.com>
2021-05-06 09:06:07 -0500

    Adds test for the apply scope function

--- src/test/kotlin/ApplyScopeFunctionTest.kt
+++ src/test/kotlin/ApplyScopeFunctionTest.kt
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class ApplyScopeFunctionTest {
+    
+    @Test
+    fun `test object configuration with the apply scope function`() {
+        val person = Person("Average Joe", 20, "", "").apply {
+            location = "Moorhead"
+            occupation = "Engineer"
+        }
+
+        assertEquals("Moorhead", person.location)
+        assertEquals("Engineer", person.occupation)
+    }
 }
